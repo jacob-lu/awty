e49f157f94f270c5140922cdbc777d4031d49afa
Jacob Lu <jacoblu23@gmail.com>
2018-05-12 22:46:39 -0700

    finished part 2

--- app/src/main/java/edu/uw/ischool/jacoblu/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/jacoblu/awty/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.uw.ischool.jacoblu.awty
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -8,6 +9,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.support.v4.app.ActivityCompat
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -18,8 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val messageInput:EditText = findViewById<EditText>(R.id.Message)
+        ActivityCompat.requestPermissions(this, arrayOf (Manifest.permission.SEND_SMS),1);
+        val messageInput:EditText = findViewById(R.id.Message) as EditText
         val phoneInput:EditText = findViewById<EditText>(R.id.Phone)
         val timeintervalInput:EditText = findViewById<EditText>(R.id.TimeInterval)
         val startOrStopButton:Button = findViewById<Button>(R.id.StartOrStop)
@@ -29,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         startOrStopButton.setOnClickListener {
             when (startOrStopButton.text) {
                "Start" -> {
-                    if (messageInput.text.isNotEmpty() && phoneInput.text.length == 10 && timeintervalInput.text.toString().toLong() > 0) {
+                    if (messageInput.text.isNotEmpty() && phoneInput.text.toString().length != 0 && timeintervalInput.text.toString().toLong() > 0) {
                         startOrStopButton.text = "Stop"
                         intent.putExtra("MessageInput", messageInput.text.toString())
                         intent.putExtra("NumberInput", phoneInput.text.toString())
@@ -40,8 +43,8 @@ class MainActivity : AppCompatActivity() {
                     } else {
                         if (!messageInput.text.isNotEmpty()) {
                             Toast.makeText(this, "Text box cannot be empty", Toast.LENGTH_LONG).show()
-                        } else if (phoneInput.text.length != 10) {
-                            Toast.makeText(this, "Phone number must be 10 digits", Toast.LENGTH_LONG).show()
+                        } else if (phoneInput.text.toString().length == 0) {
+                            Toast.makeText(this, "Phone number must have some digits", Toast.LENGTH_LONG).show()
                         } else if (timeintervalInput.text.toString().toLong() <= 0) {
                             Toast.makeText(this, "Interval must be larger than 0 minutes", Toast.LENGTH_LONG).show()
                         }
@@ -61,9 +64,11 @@ class AlarmReceiver : BroadcastReceiver(){
     override fun onReceive(context: Context, intent: Intent) {
         val number = intent.getStringExtra("NumberInput")
         val message = intent.getStringExtra("MessageInput")
-        Toast.makeText(context, "(" + number.substring(0..2) + ") " +
-                number.substring(3..5) + "-" + number.substring(6)
-                + ": " + message, Toast.LENGTH_LONG).show()
+//        Toast.makeText(context, "(" + number.substring(0..2) + ") " +
+//                number.substring(3..5) + "-" + number.substring(6)
+//                + ": " + message, Toast.LENGTH_LONG).show()
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(number, null, message, null, null)
         Log.i("mainactivity","works!")
     }
 }
